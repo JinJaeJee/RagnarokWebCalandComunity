01a58346bf25a6b987adc03d8f6200b05041d914
JinJaeJee <jpluemphiromnat@gmail.com>
2023-10-30 20:08:34 +0700

    feat: initial app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 
+import DropdownList from './components/DropdownList/DropdownList'
+import Layout from './components/Layout/Layout'
+import CalResult from './components/Wraper/CalResult'
 import './globals.css'
 
 
 const App = () => {
 
   return (
-    <div>Hi</div>
+    <Layout>
+      <CalResult />
+      <DropdownList />
+    </Layout>
   )
 }
 
